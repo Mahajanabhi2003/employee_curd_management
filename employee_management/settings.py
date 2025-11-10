16db3f2cf4b3bc901112bc9800999dcc2c6f7348
Mahajanabhi2003 <mahajanabhishe123@gmail.com>
2025-11-10 16:18:07 +0530

    Updated settings and requirements for Render deployment

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-o5^8l=)fh)la8p#azru&7!r@@#&o=lr)c@&e1_16aly2*22(s4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
